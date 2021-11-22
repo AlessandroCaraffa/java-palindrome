13891327fadbfc51c3bc6504d50f2ac1654398ae
AlessandroCaraffa <alessandro.caraffa93@gmail.com>
2021-11-22 15:34:09 +0100

    Finale in teoria

---
+++ src/org/generation/italy/strings/VerificaPalindrome.java
@@ -0,0 +1,36 @@
+package org.generation.italy.strings;
+
+import java.util.Scanner;
+
+public class VerificaPalindrome {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Qual'è la parola da analizzare?");
+		String parola = scan.nextLine();
+		String parolaBackwords ="";
+		for (int i =parola.length() -1 ; i >= 0 ;i--) {
+			parolaBackwords += parola.charAt(i) ;
+		}
+		if (parolaBackwords.equals(parola)) {
+			System.out.println("La parola è un palindromo,ed è lunga " + parola.length() + " caratteri." );
+		}
+//		System.out.println(parolaBackwords);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
